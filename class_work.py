5eaa0b282d858843e412c4f7933651d35f96ebe6
oni adedeji <onideji6@gmail.com>
2025-08-08 04:52:23 -0700

    new work

---
+++ class_work.py
@@ -0,0 +1,18 @@
+full_name = "Oni Adedeji"
+university = "Ajayi crowther university"
+local_govt_area = "yewa"
+favourite_food = "amala"
+print(f"{full_name}\n{university}\n{local_govt_area}\n{favourite_food}" )
+
+name = "deji"
+state_of_origin = "ogun state"
+print("my name is" , name , " i am from" , state_of_origin)
+
+print("monday\nfirst period:\tenglish\nsecond period:\tgovernment\nthird period:\tmath")
+
+full_name = "Oni Adedeji"
+my_class = "primary 1"
+best_subject = "math"
+print(f"i am {full_name} and i am in {my_class} my favourite subject is {best_subject}")
+
+print("""nigeria, where the sun rises on a taspestry of tribes and tongues\nher river sing of ancient kingdoms, rich with pride and song\nand her people, fierce in spirit dance through trial, unbroken and strong""")
